f680649b9d224f6124ed1835bb36bf855fda36ff
Mike <ivanov_mv@viro.edu.ru>
2023-02-02 16:53:21 +0300

    add logs

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import os, openpyxl
 
-directory = [item for item in os.listdir() if os.path.isdir(item) and True]
 
-print(directory)
+logs = open("logs.txt", "a") # open/create logs file
+logs.truncate(0) # clean logs file
+
+#get folders list that have xlsx files
+directory = [item for item in os.listdir() if os.path.isdir(item) and not item.endswith('.xlsx')]
+
+logs.write(str(directory))
+
+logs.close # close logs file
